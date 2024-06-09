9715510945597e6206ed0019559198d3468061d3
Geometary <tniu2001@gmail.com>
2024-06-09 15:01:27 +0800

    Completed and tested simulation functionalities.

--- OpticalStack.py
+++ OpticalStack.py
@@ -7,9 +7,10 @@ class OpticalStack:
     a semi-definite air layer at the top spanning z < 0, optical layers with finite thicknesses below the vacuum
     layer, and a semi-definite substrate layer extending to z=∞ at the bottom.
     '''
-    def __init__(self, wavelength, incident_angle=0):
+    def __init__(self, wavelength, initial_state=0):
         '''
-        Initializes the OpticalStack, given the wavelength in nm. Normal incidence by default.
+        Initializes the OpticalStack, given the wavelength in nm.
+        Set initial_state=0 (default) for LCP (1, i) and initial_state=1 for RCP (1, -i).
         '''
         self.n_list = np.array([1], dtype=complex)       # list of complex refractive indices n, from top to bottom
                                 # the vacuum layer with n=1 is already included
@@ -18,13 +19,13 @@ class OpticalStack:
                                 # have thickness = 0 by default.
 
         self.VBs = [0]  # list of Verdet constant times magnetic field, VB, from top to bottom layer
-                                  # in units of μrad/mm
+                                  # in units of μrad/mm = 1e-12rad/nm
 
-        self.conductivities = np.array([0], dtype=complex)  # list of longitudinal and Hall conductivities of
+        self.conductivities = np.array([0, 0], dtype=complex)  # list of longitudinal and Hall conductivities of
                                                             # each layer, in units of e^2/hbar (1/137)
 
         self.wavelength = wavelength        # wavelength in nm
-        self.incident_angle = incident_angle        # incident angle, in deg
+        self.initial_state = initial_state        # initial state. 0 for (1, i) and 1 for (1, -i).
     
         return
 
@@ -37,8 +38,8 @@ class OpticalStack:
         '''
         self.n_list = np.append(self.n_list, np.array([n]))
         self.d_list.append(d)
-        self.VBs.append(VB)
-        self.conductivities = np.append(self.conductivities, np.array([sigma_xx, sigma_xy], dtype=complex))
+        self.VBs.append(VB * 1e-12)
+        self.conductivities = np.vstack((self.conductivities, np.array([sigma_xx, sigma_xy], dtype=complex) / 137))
 
         return 0
     
@@ -58,22 +59,49 @@ class OpticalStack:
         self.M[0][0] = 1     # encodes E_0^+ = 1 in M*v = w
         curr_row = 1        # pointer for the current row of M being processed
         k = 2 * np.pi / self.wavelength       # wavevector in vacuum, in nm^-1
+        polarization = (-1) ** self.initial_state       # +1 for (1, i), -1 for (1, -i)
         for i in range(N + 1):
-            r_i_i1 = (self.n_list[i] - self.n_list[i+1]) / (self.n_list[i] + self.n_list[i+1])      # r_i_i+1
-            r_i1_i = -1 * r_i_i1                                                      # r_i+1_i
-            t_i_i1 = 2 * self.n_list[i] / (self.n_list[i] + self.n_list[i+1])         # t_i_i+1
-            t_i1_i = 2 * self.n_list[i+1] / (self.n_list[i] + self.n_list[i+1])       # t_i+1_i
-            
+            n_i, n_i1 = self.n_list[i], self.n_list[i+1]
+            sigmas_i, sigmas_i1 = self.conductivities[i], self.conductivities[i+1]
+            VB_i1 = self.VBs[i+1]       # VB of the (i+1)th layer, 
+
+            # to compute reflection coefficients r_i_i1 and r_i1_i
+            # r_i_i1 = (self.n_list[i] - self.n_list[i+1]) / (self.n_list[i] + self.n_list[i+1])      # r_i_i+1
+            # r_i1_i = -1 * r_i_i1                                                      # r_i+1_i
+            r_i_i1_xx = 1.0 / ((n_i + n_i1 + 4*np.pi*sigmas_i1[0])**2 + (4*np.pi*sigmas_i1[1])**2)
+            r_i_i1_xy = r_i_i1_xx * (-8*np.pi*n_i*sigmas_i1[1])
+            r_i_i1_xx *= n_i**2 - (n_i1 + 4*np.pi*sigmas_i1[0])**2 - (4*np.pi*sigmas_i1[1])**2
+            r_i_i1 = r_i_i1_xx + polarization * 1j * r_i_i1_xy      # r_i_i+1 = r_xx +- ir_xy
+
+            r_i1_i_xx = 1.0 / ((n_i1 + n_i + 4*np.pi*sigmas_i[0])**2 + (4*np.pi*sigmas_i[1])**2)
+            r_i1_i_xy = r_i1_i_xx * (-8*np.pi*n_i1*sigmas_i[1])
+            r_i1_i_xx *= n_i1**2 - (n_i + 4*np.pi*sigmas_i[0])**2 - (4*np.pi*sigmas_i[1])**2
+            r_i1_i = r_i1_i_xx + polarization * 1j * r_i1_i_xy      # r_i+1_i = r_xx +- ir_xy
+
+
+            # to compute transmission coefficients t_i_i1 and t_i1_i
+            # t_i_i1 = 2 * self.n_list[i] / (self.n_list[i] + self.n_list[i+1])         # t_i_i+1
+            # t_i1_i = 2 * self.n_list[i+1] / (self.n_list[i] + self.n_list[i+1])       # t_i+1_i
+            t_i_i1_xx = 1.0 / ((n_i + n_i1 + 4*np.pi*sigmas_i1[0])**2 + (4*np.pi*sigmas_i1[1])**2)
+            t_i_i1_xy = r_i_i1_xy
+            t_i_i1_xx *= 2 * n_i * (n_i + n_i1 + 4*np.pi*sigmas_i1[0])
+            t_i_i1 = t_i_i1_xx + polarization * 1j * t_i_i1_xy
+
+            t_i1_i_xx = 1.0 / ((n_i1 + n_i + 4*np.pi*sigmas_i[0])**2 + (4*np.pi*sigmas_i[1])**2)
+            t_i1_i_xy = r_i1_i_xy
+            t_i1_i_xx *= 2 * n_i1 * (n_i1 + n_i + 4*np.pi*sigmas_i[0])
+            t_i1_i = t_i1_i_xx + polarization * 1j * t_i1_i_xy
+
             
             # The three lines below describe the first equation
             self.M[curr_row][2*i] = r_i_i1
             self.M[curr_row][2*i+1] = -1
-            self.M[curr_row][2*i+3] = t_i1_i * np.exp(1j * self.n_list[i+1] * k * self.d_list[i+1])
+            self.M[curr_row][2*i+3] = t_i1_i * np.exp(1j * (self.n_list[i+1] * k + polarization * VB_i1) * self.d_list[i+1])
             curr_row += 1
             # The three lines below describe the second equation
-            self.M[curr_row][2*i] = t_i_i1 * np.exp(1j * self.n_list[i+1] * k * self.d_list[i+1])
+            self.M[curr_row][2*i] = t_i_i1 * np.exp(1j * (self.n_list[i+1] * k + polarization * VB_i1) * self.d_list[i+1])
             self.M[curr_row][2*i+2] = -1
-            self.M[curr_row][2*i+3] = r_i1_i * np.exp(2j * self.n_list[i+1] * k * self.d_list[i+1])
+            self.M[curr_row][2*i+3] = r_i1_i * np.exp(2j * (self.n_list[i+1] * k + polarization * VB_i1) * self.d_list[i+1])
             curr_row += 1
         self.M[curr_row][-1] = 1    # encodes E_{N+1}^- = 0 in M*v = w
 
--- moke_simulation.py
+++ moke_simulation.py
@@ -3,13 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy.linalg import inv
 
-def main():
+def monolayer_test(n, wavel=1550):
     '''
-    The main function to be run.
+    Test the reflection simulation against theory for a single layer with refractive index n suspended in the air,
+    for various thicknesses.
     '''
-    wavel = 1550
     k = 2*np.pi / wavel
-    n = 10
     d_vals = np.linspace(start=0, stop=wavel/2, num=100)
     R_vals_theory = np.abs((1/n - n) * np.sin(n*k*d_vals) / ((n+1/n)*np.sin(n*k*d_vals) + 2j * np.cos(n*k*d_vals)))**2
     R_vals = []
@@ -18,7 +17,6 @@ def main():
         os = OpticalStack(wavelength=wavel)
         os.insert_layer(n=n, d=d)
         os.insert_layer(n=1, d=0)
-        os.generate_M_normal()
         v = os.compute_v()
         R_vals.append(np.abs(v[1])**2)
 
@@ -30,7 +28,38 @@ def main():
     plt.title("Reflectance vs thin film thickness with n = {}".format(n))
     plt.legend()
     plt.show()
-    
+
+
+def VB_test(VB, d, wavel=1550):
+    '''
+    Test the Faraday rotation due to Faraday effect under magnetic field.
+    VB is in μrad/mm = 1e-12 rad/nm. d is in nm.
+    '''
+    theta_F = 2 * VB * 1e-12 * d        # The Faraday angle due to two propagations through the Faraday layer,
+                                        # in rad
+    k = 2 * np.pi / wavel
+    dyna_phase = 2 * k * d          # the dynamical phase due to wave propagation in n=1
+    os_l = OpticalStack(wavelength=wavel, initial_state=0)
+    os_r = OpticalStack(wavelength=wavel, initial_state=1)
+    os_l.insert_layer(n=1, d=d, VB=VB)
+    os_r.insert_layer(n=1, d=d, VB=VB)
+    os_l.insert_layer(n=1e10, d=0)      # substrate with total reflection
+    os_r.insert_layer(n=1e10, d=0)
+    vector_l, vector_r = os_l.compute_v()[:2], os_r.compute_v()[:2]
+    print("For VB={}μrad/mm, d={}nm,".format(VB, d))
+    print("LCP (1, i): ", vector_l)
+    print("where the second term should be {}".format(-1 * np.exp(1j * (dyna_phase + theta_F))))
+    print("RCP (1, -i): ", vector_r)
+    print("where the second term should be {}".format(-1 * np.exp(1j * (dyna_phase - theta_F))))
+
+    return
+
+def main():
+    '''
+    The main function to be run.
+    '''
+    # monolayer_test(1+0.01j)
+    VB_test(VB=1000, d=10e+6)
     return 0
 
 
