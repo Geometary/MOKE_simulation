92bdbeee51bc0cb9794f6a32826cc00fc8021878
Geometary <tniu2001@gmail.com>
2024-06-12 11:58:21 -0700

    Completed simulations and uploaded results, though still sus.

--- moke_simulation.py
+++ moke_simulation.py
@@ -43,14 +43,15 @@ def VB_test(VB, d, wavel=1550):
     os_r = OpticalStack(wavelength=wavel, initial_state=1)
     os_l.insert_layer(n=1, d=d, VB=VB)
     os_r.insert_layer(n=1, d=d, VB=VB)
-    os_l.insert_layer(n=1e10, d=0)      # substrate with total reflection
-    os_r.insert_layer(n=1e10, d=0)
+    os_l.insert_layer(n=1e7, d=0)      # substrate with total reflection
+    os_r.insert_layer(n=1e7, d=0)
     vector_l, vector_r = os_l.compute_v()[:2], os_r.compute_v()[:2]
-    print("For VB={}μrad/mm, d={}nm,".format(VB, d))
+    print("For VB={}μrad/mm, d={}mm,".format(VB, d/1e+6))
     print("LCP (1, i): ", vector_l)
     print("where the second term should be {}".format(-1 * np.exp(1j * (dyna_phase + theta_F))))
     print("RCP (1, -i): ", vector_r)
     print("where the second term should be {}".format(-1 * np.exp(1j * (dyna_phase - theta_F))))
+    print("and the resultant Kerr angle is {:.2f} μrad.".format(np.angle(vector_l[1] / vector_r[1]) / 2 * 1e+6))
 
 
 def compute_twisted_stack(n_bscco, n_SiO2, n_Si, d1, d2, d_SiO2, sigma_xx, sigma_xy):
@@ -67,10 +68,12 @@ def compute_twisted_stack(n_bscco, n_SiO2, n_Si, d1, d2, d_SiO2, sigma_xx, sigma
         '''
         assert isinstance(stack, OpticalStack)
         stack.insert_layer(n=n_bscco, d=d1, sigma_xx=sigma_xx * d1 * 1e-7 / 2.4316e-4)
-        stack.insert_layer(n=n_bscco, d=3.2, sigma_xx=sigma_xx * 3.2 * 1e-7 / 2.4316e-4, sigma_xy=sigma_xy)
+        stack.insert_layer(n=1, d=3.2, sigma_xx=sigma_xx * 3.2 * 1e-7 / 2.4316e-4, sigma_xy=sigma_xy)       # ignore the dielectric
+                                                                                                                  # env of interface
         stack.insert_layer(n=n_bscco, d=d2, sigma_xx=sigma_xx * d2 * 1e-7 / 2.4316e-4)
         stack.insert_layer(n=n_SiO2, d=d_SiO2)
         stack.insert_layer(n=n_Si, d=0)
+        
         return stack
     
     lcp_stack = build_stack(OpticalStack(wavelength=1550, initial_state=0))
@@ -79,12 +82,51 @@ def compute_twisted_stack(n_bscco, n_SiO2, n_Si, d1, d2, d_SiO2, sigma_xx, sigma
     rcp_vector = rcp_stack.compute_v()[:2]
     return lcp_vector, rcp_vector
 
+
+def twisted_stack_test(d1, d2, d_SiO2, interface_sigma_xy):
+    '''
+    Simulate the reflection upon a twisted BSCCO stack with two BSCCO films of thicknesses d1, d2 (in nm).
+    The sigma_xy of the twisted interface is in units of e^2/hbar.
+    Returns the measured Kerr angle (half the phase difference) in μrad and reflectance of RCP (~LCP).
+    '''
+    n_bscco = np.sqrt(-0.846 + 2.212j)
+    n_SiO2 = 1.45
+    n_Si = 3.45
+    bscco_sigma_xx = 700 * (-1j) ** (1.447 - 2)     # longitudinal conductivity of bscco in units of Ω^-1cm^-1
+    l_vector, r_vector = compute_twisted_stack(n_bscco=n_bscco, n_SiO2=n_SiO2, n_Si=n_Si, d1=d1, d2=d2, 
+                                               d_SiO2=d_SiO2, sigma_xx=bscco_sigma_xx, sigma_xy=interface_sigma_xy)
+    r = l_vector[1] / r_vector[1]
+    reflectance_rcp = np.abs(r_vector[1]) ** 2
+    # print("The magnitude of r (LCP/RCP) is {:.4f} and the phase of r is {:.4f} μrad, reading θ_K = {:.4f} μrad.".format(np.abs(r), np.angle(r) * 1e6, np.angle(r) * 1e6 / 2))
+    sigma_xx, sigma_xy = bscco_sigma_xx * 3.2e-7 / 2.4316e-4 / 137, interface_sigma_xy / 137
+    kerr_angle_theory = np.real(np.arctan(sigma_xy / (sigma_xx + 2*np.pi*(sigma_xx**2 + sigma_xy**2))))
+    # print("The theoretical Kerr angle is {:.4f} μrad.".format(kerr_angle_theory * 1e6))
+    # print("Reflectance of RCP is {:.4f}.".format(reflectance_rcp))
+
+    return np.angle(r) * 1e6 / 2, reflectance_rcp
+
 def main():
     '''
     The main function to be run.
     '''
     # monolayer_test(1+0.01j)
-    VB_test(VB=376.2, d=7e+7)
+    # VB_test(VB=10, d=2e+6)
+    d1_vals = np.linspace(start=2, stop=50, num=100)
+    kerr_angles = []
+    reflectances = []
+    for d1 in d1_vals:
+        ang, ref = twisted_stack_test(d1=d1, d2=30, d_SiO2=280, interface_sigma_xy=1e-4)
+        kerr_angles.append(ang)
+        reflectances.append(ref)
+    # twisted_stack_test(d1=10, d2=10, d_SiO2=10, interface_sigma_xy=1e-4)
+
+    plt.xlabel("Top layer thickness (nm)")
+    plt.plot(d1_vals, kerr_angles, label="Kerr angle")
+    plt.plot(d1_vals, reflectances, label="Reflectance")
+    plt.title("d2=30nm, d_SiO2=280nm, σ_xy=1e-4 / 137")
+    plt.legend()
+    plt.show()
+
     return 0
 
 
